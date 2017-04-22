4bde6bfbf5d67ab3b4c53bce674c6bf07a606688
Plomboe <m.tareq.karimi@gmail.com>
2017-04-22 02:02:16 -0400

    SpecialElements

---
+++ CheckBox/EntryPoint.cs
@@ -0,0 +1,43 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Threading;
+
+class EntryPoint
+{
+    static IWebDriver driver = new ChromeDriver();
+    static IWebElement checkBox;
+
+    static void Main(string[] args)
+    {
+        string url = "http://testing.todvachev.com/special-elements/check-button-test-3/";
+        string option = "3";//the number of the checkbox
+
+        
+
+
+        driver.Navigate().GoToUrl(url);
+
+        checkBox = driver.FindElement(By.CssSelector("#post-33 > div > p:nth-child(8) > input[type=\"checkbox\"]:nth-child(" + option + ")"));
+
+        //checkbox.Click(); //Clicks the checkbox, can do this for logging in
+
+        //Thread.Sleep(5000);
+
+
+        if (checkBox.GetAttribute("checked") == "true")//when the check box is checked, html will say checked
+        {
+            Console.WriteLine("Checkbox is checked!");
+        }
+        else
+        {
+            Console.WriteLine("The checkbox is NOT checked");
+
+        }
+
+        driver.Quit();
+
+
+    }
+}
+
---
+++ SpecialElements/EntryPoint.cs
@@ -0,0 +1,34 @@
+﻿
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Threading;
+
+class EntryPoint
+{
+    static IWebDriver driver = new ChromeDriver();
+    static IWebElement textBox;//Can't initialize here, cant do FindElement here, it has to come after url
+
+    static void Main()
+    {
+        string url = "http://testing.todvachev.com/special-elements/text-input-field/";
+        
+
+        driver.Navigate().GoToUrl(url);
+
+        textBox = driver.FindElement(By.Name("username"));
+
+        textBox.SendKeys("Test text");
+
+        Thread.Sleep(3000); //keeps browser on for 3000 milliseconds 
+
+        //textBox.Clear();//Deletes text you just input
+        //Console.WriteLine(textBox.GetAttribute("value"));//Input Text boxes always go under the string: value(value = whatever is inside the text)
+        Console.WriteLine(textBox.GetAttribute("maxlength"));//Finds the max amount of chars allowed
+        
+        Thread.Sleep(3000);
+
+        driver.Quit();
+    }
+}
+
